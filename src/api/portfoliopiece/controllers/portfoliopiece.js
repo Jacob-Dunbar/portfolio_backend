e535b42a87a3734b472ee6982d701cd14bd1642e
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-29 18:06:50 +0100

    first commit

---
+++ src/api/portfoliopiece/controllers/portfoliopiece.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfoliopiece controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::portfoliopiece.portfoliopiece');
---
+++ src/api/portfoliopiece/services/portfoliopiece.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfoliopiece service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::portfoliopiece.portfoliopiece');
