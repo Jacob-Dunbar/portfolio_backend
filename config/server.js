d2fcbe3d03776a05a6a1ffe584a402dcc64a5bf2
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-29 18:54:49 +0100

    fix: heroku

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "hPmgEaK8BXPSDKp7ze+3iA==",
+      "iqgREiueRevSy2hmqHjxFA==",
+      "zHYKpTOnjOXvCrpixhx+Hg==",
+      "vyPTvtXx1Xf3bwrhwXwbdA==",
+    ],
   },
 });
