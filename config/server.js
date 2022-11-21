83b023b259f3062c7302e4559bbc3a8affb5801d
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-11-21 19:09:44 +0000

    doc: server comments

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
+    // had to add these manualy as the env pointer didnt seem to work
     keys: [
       "hPmgEaK8BXPSDKp7ze+3iA==",
       "iqgREiueRevSy2hmqHjxFA==",
